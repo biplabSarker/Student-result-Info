da292d34d6a81e87c87d425af5f909cbc8d49e34
biplabSarker <Skbiplab143@gmail.com>
2014-09-06 10:24:07 -0700

    login class
    complete login panel class

---
+++ StudentResultInfo/Model/LoginUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Model
+{
+   public class LoginUser
+    {
+        public DataSet getUser(string UserName, string Passward)
+        {
+            string Connectionstring = "Data Source=.\\SQLEXPRESS;AttachDbFilename=c:\\users\\biplab\\documents\\visual studio 2010\\Projects\\Software Engineering Project\\View\\Database1.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection objConnection = new SqlConnection(Connectionstring);
+            objConnection.Open();
+            string Commandestring = "Select * from Login where UserName='"+UserName+"' and Passward='"+Passward+"'";
+            SqlCommand objCommande = new SqlCommand(Commandestring, objConnection);
+            DataSet objDataSet = new DataSet();
+            SqlDataAdapter objAdapter = new SqlDataAdapter(objCommande);
+            objAdapter.Fill(objDataSet);
+
+            objConnection.Close();
+            return objDataSet;
+        }
+    }
+}
