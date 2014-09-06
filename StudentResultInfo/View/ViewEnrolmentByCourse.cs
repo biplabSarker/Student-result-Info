5292b6c6e62b570badb4e51c105d25d3bb1fd09b
biplabSarker <Skbiplab143@gmail.com>
2014-09-06 10:42:57 -0700

    Enrolment view
    completw display enrolment ui using enrolment class

---
+++ StudentResultInfo/View/ViewEnrolmentByCourse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Model;
+
+namespace View
+{
+    public partial class ViewEnrolmentByCourse : Form
+    {
+        public ViewEnrolmentByCourse()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewEnrolmentByCourse_Load(object sender, EventArgs e)
+        {
+            Enrolment objEnrolment = new Enrolment();
+            DataSet objDataset = objEnrolment.showEnrolByCourse();
+            dataGridView1.DataSource = objDataset.Tables[0];
+            
+        }
+
+       
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ StudentResultInfo/View/ViewEnrolmentByStudent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Model;
+
+namespace View
+{
+    public partial class ViewEnrolmentByStudent : Form
+    {
+        public ViewEnrolmentByStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewEnrolmentByStudent_Load(object sender, EventArgs e)
+        {
+            Enrolment objEnrolment = new Enrolment();
+            DataSet objDataset = objEnrolment.showEnrolByStudent();
+            dataGridView1.DataSource = objDataset.Tables[0];
+        }
+    }
+}
